184102e458bcba473b3ab013424208bc699dbe0a
Lyney <30753183+hanlim83@users.noreply.github.com>
2023-10-02 16:00:46 +0800

    Base Vue Template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import materialKit from "./material-kit";
 
+const app = createApp(App);
+app.use(createPinia());
+app.use(materialKit);
 createApp(App).use(router).mount("#app");
