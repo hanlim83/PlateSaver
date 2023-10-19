3ddf53a08fd908e33643c1e2c7a9029d1c517a66
Tntninja1 <jiawei.ang.2022@scis.smu.edu.sg>
2023-10-19 12:40:42 +0800

    add post backend

---
+++ backend/models/posts.js
@@ -0,0 +1,25 @@
+const { firebase } = require("../utils/firebase");
+
+async function createPost(data) {
+    // To do more error handling here
+    return new Promise((resolve) => {
+        var db = firebase.database();
+        var ref = db.ref("/Posts");  //Set the current directory you are working in
+        ref.push(data);
+        resolve("Success");
+    });
+}
+
+async function getPosts() {
+    return new Promise((resolve) => {
+        var db = firebase.database();
+        var ref = db.ref("/Posts");  //Set the current directory you are working in
+        ref.once("value", function (snapshot) {
+            var response = snapshot.val();
+            resolve(response);
+        });
+
+    });
+}
+
+module.exports = { createPost, getPosts };
