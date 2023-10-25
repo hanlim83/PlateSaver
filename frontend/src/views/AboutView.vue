6a1de509f083128458e94517f357d51c5f14ae44
raellusionz <146325019+raellusionz@users.noreply.github.com>
2023-10-25 21:55:15 +0800

    Update AboutView.vue

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class = "row justify-content-center p-3">
+        <img src="@/assets/logo.png" style="width:40%;height:;">      
+    </div>
+
+    <div class = "row p-3 justify-content-center">
+      <div class = "col-7 col-md-4 text-center">
+        PlateSaver aims to inspire, educate, and empower individuals and communities to reduce food waste through sharing sustainable practices in a community-centered food-sharing blog. 
+      </div>
+    </div>
+
+
+    <div class = "row p-3 justify-content-center text-center">
+        <div class = "col-9">
+          <h2>Proudly Aligned with United Nations Sustainable Development Goals</h2>
+      </div>
+    </div>
+    <div class = "row justify-content-center p-3">
+      <img src="@/assets/SDG3.png" style="width:30%;height:auto;">   
+      <img src="@/assets/SDG4.png" style="width:30%;height:auto;">   
+      <img src="@/assets/SDG12.png" style="width:30%;height:auto;">   
+    </div>
   </div>
+
 </template>
