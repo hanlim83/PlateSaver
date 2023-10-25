ed07b9541f8db1a675fee6913d24c32a0a1c8741
h834 <aujanweng@gmail.com>
2023-10-25 19:38:35 +0800

    Update CreatePostView.vue

--- frontend/src/views/posts/CreatePostView.vue
+++ frontend/src/views/posts/CreatePostView.vue
@@ -1,26 +1,59 @@
+<style>
+.centerCropped {
+  object-fit: none; /* Do not scale the image */
+  object-position: center; /* Center the image within the element */
+  height: 100px;
+  width: 100%;
+}
+</style>
+
 <template>
   <div class="container-xxl display-3 text-center text-primary"><strong>Share your Food With Others</strong></div>
   <br/>
   <br/>
   <div class="create-post">
-    <form>
-      <div class="mb-3">
-        <input type="text" class="form-control" value="title" />
+      <div class="flex mx-auto display-2 text-center text-dark fw-bold">
+        <img src="@/assets/frontpage.jpeg" class="centerCropped ">
+        {{ titleHeaderforPost }}
+      </div>
+    <br/>
+    <form >
+
+
+      <div class="row g-3">
+      <div class="col"></div>
+
+      <div class="col-4 mb-3">
+        <input type="text" class="form-control text-center" v-model="titleHeaderforPost" placeholder="Title" id="title"/>
       </div>
 
-      <div class="mb-3">
-        <label class="form-label" for="customFile">Upload</label>
-        <input type="file" class="form-control" id="customFile">
+      <div class="col"></div>
       </div>
+
+
+      
       <div class="mb-3">
-        <label for="food" class="form-label">Description:</label>
-        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example" id="food">
+        <label for="description" class="form-label">Description:</label>
+        <textarea class="form-control" id="description" rows="3"></textarea>
       </div>
-      <div class="mb-3">
-        <label for="Tags" class="form-label">Tags:</label>
-        <input type="text" class="form-control" id="food" />
+
+
+      <div class="row">
+        <div class="col"></div>
+
+        <div class="col-4 mb-3 ">
+          <label class="form-label mb-0" for="customFile">Upload An Image:</label>
+          <input type="file" class="form-control" id="customFile">
+        </div>
+        <div class="col-4 mb-3">
+          <label for="Tags" class="form-label mb-0">Hashtags:</label>
+          <input type="text" class="form-control" id="food" />
+        </div>
+
+        <div class="col"></div>
       </div>
-      <button type="submit" class="btn btn-primary" @click="createPost()">Submit</button>
+        <button type="submit" class="btn btn-primary" @click="createPost()">Submit</button>
+      
     </form>
 
     <br />
@@ -45,7 +78,8 @@ export default {
       timeStamp: '',
       location: '',
       tags: '',
-      collectionStatus: true
+      collectionStatus: true,
+      titleHeaderforPost:""
     }
   },
   methods: {
@@ -61,8 +95,10 @@ export default {
         tags: this.tag,
         status: true
       })
+
       console.log('Response: ', response)
-    }
+    },
+
   }
 }
 </script>
