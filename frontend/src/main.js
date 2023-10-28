fa453f0287866217e42a78de29114f204d05bd00
Lyney <30753183+hanlim83@users.noreply.github.com>
2023-10-28 23:30:19 +0800

    Basic Authentication Up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,31 +15,52 @@ import 'aos/dist/aos.css'
 import globalComponent from './plugins/global-components'
 import globalDirective from './plugins/global-directive'
 import globalMixin from './plugins/global-mixin'
-
 require('waypoints/lib/noframework.waypoints.min')
 
+//Toastify
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
+//VueFire
 import { VueFire, VueFireAuth } from 'vuefire'
 import { firebaseApp } from './firebase'
 
-const app = createApp(App);
-app.use(store).use(router);
+const app = createApp(App)
+app.use(store).use(router)
 
+//VueFire
 app.use(VueFire, {
   firebaseApp,
   modules: [VueFireAuth()]
-});
+})
 
 // Library Components
-app.use(VueSweetalert2);
-app.use(VueApexCharts);
-app.use(BootstrapVue3);
-app.component('counter-up', CounterUp);
+app.use(VueSweetalert2)
+app.use(VueApexCharts)
+app.use(BootstrapVue3)
+app.component('counter-up', CounterUp)
 
 // Custom Components & Directives
-app.use(globalComponent);
-app.use(globalDirective);
-app.mixin(globalMixin);
+app.use(globalComponent)
+app.use(globalDirective)
+app.mixin(globalMixin)
 
-app.mount('#app');
+//Toastify
 
-export default app;
+app.use(Vue3Toastify, {
+  position: 'top-right',
+  duration: 3000,
+  dismissible: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false,
+  theme: 'light'
+})
+
+app.mount('#app')
+
+export default app
