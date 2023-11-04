ab23583b0fdd6d5b5d767be21c46776e00b3f243
Lyney <30753183+hanlim83@users.noreply.github.com>
2023-11-05 02:34:06 +0800

    Updated Posts

--- frontend/src/views/posts/CreatePostView.vue
+++ frontend/src/views/posts/CreatePostView.vue
@@ -7,63 +7,56 @@ input[type=text], input[type=file] {
 
 <template>
   <div class="container-xxl display-3 text-center text-primary"><strong>Share your Food With Others!</strong></div>
-  <br/>
-  <br/>
+  <br />
+  <br />
   <div class="create-post">
-
-
     <div class="card text-bg-dark">
-      <img src="@/assets/fried_rice_banner.png">
+      <img src="@/assets/fried_rice_banner.png" />
       <div class="card-img-overlay">
         <div class="card-title display-5 text-left align-middle text-dark fw-bold w-50 h-100 overflow-auto text-capitalize">
           {{ title }}
         </div>
       </div>
     </div>
-    <br/>
-    <form>
-
-
+    <br />
+    <form @submit.prevent="handleAddingPost">
       <div class="row g-3">
-      <div class="col"></div>
-
-      <div class="col-4 mb-3">
-        <input type="text" class="form-control text-center" v-model="title" placeholder="Title" id="title"/>
+        <div class="col"></div>
+        <div class="col-4 mb-3">
+          <label for="title" class="form-label">Title:</label>
+          <input type="text" class="form-control text-center" v-model="title" placeholder="Title" id="title" required />
+        </div>
+        <div class="col"></div>
       </div>
 
-      <div class="col"></div>
-      </div>
-
-
-      
       <div class="mb-3">
         <label for="description" class="form-label">Description:</label>
-        <textarea class="form-control" id="description" rows="3" v-model="content"></textarea>
+        <textarea class="form-control" id="description" rows="3" v-model="content" required></textarea>
       </div>
 
-
       <div class="row">
-
         <div class="col-4 mb-3">
           <label class="form-label mb-0" for="contact">Contact:</label>
-          <input type="text" class="form-control" id="contact">
+          <input type="text" class="form-control" id="contact" required />
         </div>
 
-        <div class="col-4 mb-3 ">
+        <div class="col-4 mb-3">
           <label class="form-label mb-0" for="customFile">Upload An Image:</label>
-          <input type="file" class="form-control" id="customFile">
+          <button type="button" class="form-control" id="customFile" @click="open({ accept: 'image/*', multiple: false })">Choose Image</button>
         </div>
-
 
         <div class="col-4 mb-3">
           <label for="Tags" class="form-label mb-0">Hashtags:</label>
-          <input type="text" class="form-control" id="food" v-model="tags"/>
+          <input type="text" class="form-control" id="food" v-model="tags" required />
         </div>
-
+        <div class="col-4 mb-3">
+          <h4>Your Position</h4>
+          Latitude : {{ currPos.lat.toFixed(2) }}, longitude {{ currPos.lng.toFixed(2) }}
+        </div>
+        <div ref="mapDiv" style="width: 100%; height: 25vh"></div>
         <div class="col"></div>
       </div>
-        <button type="submit" class="btn btn-primary" @click="createPost()">Submit</button>
-      
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
     <br />
@@ -71,51 +64,125 @@ input[type=text], input[type=file] {
   </div>
 </template>
 
-<script>
+<script setup>
+/*eslint-disable no-undef*/
+import { ref as dbRef, set } from 'firebase/database'
+import { useFirebaseAuth, useDatabase, useFirebaseStorage } from 'vuefire'
+import { ref as storageRef, uploadBytes } from 'firebase/storage'
+import { computed, ref, onMounted, watch } from 'vue'
+import router from '@/router'
+import { toast } from 'vue3-toastify'
+import { useFileDialog } from '@vueuse/core'
+import { useGeoLocation } from '@/components/useGeolocation'
+import { Loader } from '@googlemaps/js-api-loader'
 
+const auth = useFirebaseAuth()
+const storage = useFirebaseStorage()
+const db = useDatabase()
+const id = (Math.random() + 1).toString(36).substring(2)
+const { files, open } = useFileDialog()
+const title = ref('')
+const content = ref('')
+const tags = ref('')
+const contact = ref('')
+const location = ref('')
+var imagePath = ''
 
-const API_URL = 'http://localhost:4000/'
-import axios from 'axios'
+watch(files, (newFile) => {
+  console.log(newFile)
+  if (newFile.length == 1) {
+    console.log(newFile[0])
+    let newPostPicRef = storageRef(storage, 'posts-media/' + id + '/' + newFile[0].name)
+    uploadBytes(newPostPicRef, newFile[0]).then((snapshot) => {
+      console.log(snapshot)
+      imagePath = snapshot.ref.fullPath
+    })
+  }
+})
 
-export default {
-  name: 'CreatePostView',
-  data() {
-    return {
-      title: '',
-      foodImage: '',
-      content: '',
-      userID: '',
-      timeStamp: '',
-      location: '',
-      tags: [],
-      contact:'',
+const handleAddingPost = () => {
+  console.log('Adding Post')
+  try {
+    set(dbRef(db, 'Posts/' + id), {
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split('#').slice(1),
+      contact: contact.value,
+      foodImage: imagePath,
+      userID: auth.currentUser.uid,
+      timeStamp: new Date().toString(),
+      location: location.value,
       collectionStatus: true,
-      
-    }
-  },
-  methods: {
-    async createPost() {
-      // Get the day the post was made
-      var today = new Date();
-      var now = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-      this.timeStamp=now;
-      
-      
-      let response = await axios.post(API_URL + 'post', {
-        title: this.title,
-        foodImage: this.foodImage,
-        content: this.content,
-        userID: this.userID,
-        timeStamp: this.timeStamp,
-        location: this.location,
-        tags: this.tags.split("#").slice(1),
-        contact: this.contact,
-        collectionStatus: this.collectionStatus
-      });
-      console.log('post: ', this.post)
-      console.log('Response: ', response);
-    },
-
+      lat: currPos.value.lat,
+      long: currPos.value.lng
+    })
+    toast('Post Created Successfully', {
+      autoClose: 5000,
+      type: 'success'
+    })
+    setTimeout(() => {
+      router.push({ name: 'posts.myposts' })
+    }, 6000)
+  } catch (error) {
+    console.log(error)
+    toast('An Error Occurred', {
+      autoClose: 5000,
+      type: 'error'
+    })
   }
 }
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyBHv4JD1OQx8TgqAgiing9nRP6HM72zAB4'
+const { coords } = useGeoLocation() // Need Replace this with coordinates of backend
+const currPos = computed(() => ({
+  lat: coords.value.latitude,
+  lng: coords.value.longitude
+}))
+const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
+const mapDiv = ref(null)
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+onMounted(async () => {
+  await loader.load()
+  console.log('centering map on coords')
+  while (currPos.value.lat == 0 && currPos.value.lng == 0) {
+    console.log('Awaiting Coords')
+    await sleep(1000)
+  }
+  console.log('Centering Lat: ' + currPos.value.lat)
+  console.log('Centering Lng: ' + currPos.value.lng)
+
+  const map = new google.maps.Map(mapDiv.value, {
+    center: currPos.value /* centre the map */,
+    zoom: 18 /* how zoomed in is the map */
+  })
+  const icon = {
+    url: 'http://maps.gstatic.com/mapfiles/markers2/measle_blue.png', // url
+    scaledSize: new google.maps.Size(14, 14) // scaled size
+  }
+
+  const infowindow = new google.maps.InfoWindow()
+  const geocoder = new google.maps.Geocoder()
+  geocoder
+    .geocode({ location: currPos.value })
+    .then((response) => {
+      if (response.results[0]) {
+        const marker = new google.maps.Marker({
+          position: currPos.value,
+          map: map,
+          icon: icon
+        })
+        infowindow.setContent(response.results[0].formatted_address)
+        infowindow.open(map, marker) //Don't Need for Radar Page
+      } else {
+        window.alert('No results found')
+      }
+    })
+    .catch((e) => window.alert('Geocoder failed due to: ' + e))
+})
+
+toast.clearAll()
 </script>
--- frontend/src/views/posts/MyPostsView.vue
+++ frontend/src/views/posts/MyPostsView.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { useFirebaseAuth, useDatabaseList, useDatabase } from 'vuefire'
+import { ref as dbRef } from 'firebase/database'
+import IndivPost from '@/components/IndivPost.vue'
+import { computed } from 'vue'
+const auth = useFirebaseAuth()
+const db = useDatabase()
+const allposts = useDatabaseList(dbRef(db, 'Posts'))
+const myposts = computed(() => {
+  console.log(myposts)
+  return allposts.value.filter((item) => item.userID == auth.currentUser.uid)
+})
+</script>
 <template>
-    <h1> All my posts</h1>
+  <div class="row" data-masonry='{"percentPosition": true }'>
+    <div class="inner-box">
+      <div class="container">
+        <b-card-group columns>
+          <IndivPost v-for="(post, ind) in myposts" :key="ind" :id="ind" :foodImage="post.foodImage" :timeStamp="post.timeStamp" :title="post.title" :userID="post.userID" :content="post.content" :tags="post.tags" />
+        </b-card-group>
+      </div>
+    </div>
+  </div>
 </template>
