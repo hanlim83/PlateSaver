fb475f0ab345184e1f6c389535daef1faced9f65
Lyney <30753183+hanlim83@users.noreply.github.com>
2023-11-04 22:49:48 +0800

    Update HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,53 +1,12 @@
-<template>
-  <div class="home">
-    <h1>Welcome to PlateSaver</h1>
-    <div class="outerlayer">
-      <img src="@/assets/frontpage.jpeg" style="width: 100%" />
-      <div class="h1 word text-responsive">Number Of Unsung Heroes To Date</div>
-      <div class="box" style="left: 10%">{{ tenMillionsPlace }}</div>
-      <div class="box" style="left: 20%">{{ millionsPlace }}</div>
-      <div class="box" style="left: 30%">{{ hundredThousandsPlace }}</div>
-      <div class="box" style="left: 40%">{{ tenThousandsPlace }}</div>
-      <div class="box" style="left: 50%">{{ thousandsPlace }}</div>
-      <div class="box" style="left: 60%">{{ hundredsPlace }}</div>
-      <div class="box" style="left: 70%">{{ tensPlace }}</div>
-      <div class="box" style="left: 80%">{{ onesPlace }}</div>
-    </div>
-  </div>
-</template>
 <script setup>
 import { useRoute } from 'vue-router'
 import { toast } from 'vue3-toastify'
-import { ref } from 'vue'
-import { ref as dbRef, onValue } from 'firebase/database'
-import { useDatabase } from 'vuefire'
+import { useDatabaseList, useDatabase } from 'vuefire'
+import { ref as dbRef } from 'firebase/database'
+
 const route = useRoute()
-const onesPlace = ref(0)
-const tensPlace = ref(0)
-const hundredsPlace = ref(0)
-const thousandsPlace = ref(0)
-const tenThousandsPlace = ref(0)
-const hundredThousandsPlace = ref(0)
-const millionsPlace = ref(0)
-const tenMillionsPlace = ref(0)
 const db = useDatabase()
-const itemCount = ref(0)
-
-setInterval(() => {
-  onesPlace.value = Math.floor(Math.random() * 10)
-  tensPlace.value = Math.floor(Math.random() * 10)
-  hundredsPlace.value = Math.floor(Math.random() * 10)
-  thousandsPlace.value = Math.floor(Math.random() * 10)
-  tenThousandsPlace.value = Math.floor(Math.random() * 10)
-  hundredThousandsPlace.value = Math.floor(Math.random() * 10)
-  millionsPlace.value = Math.floor(Math.random() * 10)
-  tenMillionsPlace.value = Math.floor(Math.random() * 10)
-}, 1000)
-
-onValue(dbRef(db, '/users/'), (snapshot) => {
-  console.log(snapshot.val())
-  itemCount.value = Object.keys(snapshot.val()).length
-})
+const allusers = useDatabaseList(dbRef(db, 'users'))
 
 if (route.query.state != null && route.query.state === 'logout') {
   toast('Logged Out Successfully', {
@@ -66,3 +25,22 @@ if (route.query.state != null && route.query.state === 'logout') {
   })
 }
 </script>
+<template>
+  <div class="home">
+    <h1>Welcome to Your Vue.js App</h1>
+
+    <div class="outerlayer">
+      <img src="@/assets/frontpage.jpeg" style="width: 100%" />
+      <div class="h1 word text-responsive">Number Of Unsung Heroes To Date</div>
+      <div class="box" style="left: 15%" v-if="allusers.length > 1000">{{ parseInt(allusers.length.toString()[0]) % 10 }}</div>
+      <div class="box" style="left: 15%" v-else>0</div>
+      <div class="box" style="left: 30%" v-if="allusers.length > 1000">{{ parseInt(allusers.length.toString()[0]) % 10 }}</div>
+      <div class="box" style="left: 30%" v-else>0</div>
+      <div class="box" style="left: 45%" v-if="allusers.length > 100">{{ parseInt(allusers.length.toString()[0]) % 10 }}</div>
+      <div class="box" style="left: 45%" v-else>0</div>
+      <div class="box" style="left: 60%" v-if="allusers.length > 10">{{ parseInt(allusers.length.toString()[0]) % 10 }}</div>
+      <div class="box" style="left: 60%" v-else>0</div>
+      <div class="box" style="left: 75%">{{ allusers.length % 10 }}</div>
+    </div>
+  </div>
+</template>
