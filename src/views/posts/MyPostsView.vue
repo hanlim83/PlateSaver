cf22d87771af711e663347adef781472042d402e
raellusionz <146325019+raellusionz@users.noreply.github.com>
2023-11-08 15:02:12 +0800

    Align myPosts Page

--- src/views/posts/MyPostsView.vue
+++ src/views/posts/MyPostsView.vue
@@ -17,7 +17,7 @@ const myposts = computed(() => {
     <div class="inner-box">
       <div class="container">
         <div class="row">
-          <div class="col-md-6 col-lg-4" v-for="(post, ind) in myposts" :key="ind">
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch" style="max-width:400px" v-for="(post, ind) in myposts" :key="ind">
             <IndivPost :id="post.id" :foodImage="post.foodImage" :timeStamp="post.timeStamp" :title="post.title"
               :userID="post.userID" :content="post.content" :tags="post.tags" :is-collected="post.collectionStatus" />
           </div>
